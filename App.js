e5e0c771dc4e76ab721d8e07e09d863c26235f58
kaviya <kaviyachuddkie07@gmail.com>
2022-06-08 08:41:10 +0530

    Basic

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {View , Text,StyleSheet} from 'react-native';
+export default function App(){
+    return(
+        <View style={styles.task_container}> 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+            <Text style={[styles.tasks , styles.odd_tasks]}>
+                Task 1 
+            </Text>
+            <Text style={[styles.tasks , styles.even_tasks]}>
+                Task 2
+            </Text>
+            <Text style={[styles.tasks , styles.odd_tasks]}>
+                Task 3
+            </Text>
+            <Text style={[styles.tasks , styles.even_tasks]}>
+                Task 4
+            </Text>
+        </View>
+    )
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    task_container:{
+        margin: 50
+    },
+    tasks:{
+        borderWidth:2, // border : 1px solid black in react js
+        margin:20,
+        padding:15,
+        paddingLeft:20,
+        color:'black'
+
+    },
+    odd_tasks:{
+        // borderStyle:'dotted',
+        borderColor:'pink',
+        backgroundColor:'pink'
+
+    },
+    even_tasks:{
+        // borderStyle:'dashed',
+        borderColor:'#ffdab9',
+        backgroundColor:'#ffdab9'
+
+    }
 });
