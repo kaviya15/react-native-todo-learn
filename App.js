9e75d7f658c9415c966326c25847060e5a251240
kaviya <kaviyachuddkie07@gmail.com>
2022-06-08 08:45:11 +0530

    state update

--- App.js
+++ App.js
@@ -1,21 +1,67 @@
-import React from 'react';
-import {View , Text,StyleSheet} from 'react-native';
+import React , {useState} from 'react';
+import {View , Text,StyleSheet,TextInput,Button} from 'react-native';
 export default function App(){
+    const [tasks , setTasks] = useState(["Task1" , "Task2" , "Task3" , "Task4"]);
+    const [newTask , setNewTask] = useState("");
+    
+    function setTaskname (value){
+        setNewTask(value)
+        console.log(value , newTask);
+    }
+    function addEvent(){
+        console.log("clicked",tasks);
+        setTasks([...tasks,newTask])
+        setNewTask("")
+
+    }
     return(
         <View style={styles.task_container}> 
+            {
+                tasks.map((ele,index)=>{
+                    return(
+                        <Text style={ [ styles.tasks , index%2 == 0 ? styles.even_tasks :styles.odd_tasks  ]} key={ele}>
+                            {ele}
+                        </Text>
+                    )
+                })
+            }
+    {/**
+     * 
+     * Add task to the state
+     */}
 
-            <Text style={[styles.tasks , styles.odd_tasks]}>
-                Task 1 
-            </Text>
-            <Text style={[styles.tasks , styles.even_tasks]}>
-                Task 2
-            </Text>
-            <Text style={[styles.tasks , styles.odd_tasks]}>
-                Task 3
-            </Text>
-            <Text style={[styles.tasks , styles.even_tasks]}>
-                Task 4
-            </Text>
+     {/**
+      * onChangeText function outside the box 
+      */
+      }
+        {/* <View>
+            <TextInput
+            style={{margin:20,padding:15}}
+            placeholder='Enter the Task'
+            onChangeText={setTaskname}
+            value={newTask}
+            />
+        <Button title='Add Task' onPress={addEvent}/>
+        </View> */}
+
+      {/**
+       * 
+       * onChangeText function with in the component 
+       */}
+        <View>
+            <TextInput
+            onChangeText={value=>setNewTask(value)}
+            value={newTask}
+            placeholder="Enter a new Task"
+            style={{margin:20 , padding:15}}
+            />
+            <Button onPress={()=> { 
+                setTasks([...tasks , newTask]);
+                setTaskname("")
+                
+                } } title="Add new Task"/>
+        </View>
+
         </View>
     )
 }
